327aa4c85e8ac9b95fe33b03e004d160e2376342
DrJed1 <idrummerm@gmail.com>
2023-07-14 23:11:14 +0300

    version 0.1

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list")
+const headerHeight = document.querySelector("header")
+
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+  headerHeight.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navList.classList.remove("active");
+  headerHeight.classList.remove("active");
+}))
